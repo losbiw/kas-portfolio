2f0c67fab2235f2516f79ec49d0d6655b1775ba9
losbiw <skorinov41@gmail.com>
2021-07-20 01:55:41 +0300

    Add current link display to navbar

--- features/navbar/Navbar.tsx
+++ features/navbar/Navbar.tsx
@@ -1,23 +1,33 @@
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import Logo from './Logo';
 import NavLink from './NavLink';
+import Menu from '../../public/icons/menu.svg';
 
 const links = ['', 'portfolio', 'contact'];
 
-const Navbar: FC = () => (
-  <nav className="container flex justify-between pt-16">
-    <Logo bold="nefarious" thin=".kas" />
+const Navbar: FC = () => {
+  const router = useRouter();
 
-    <div className="flex">
-      {
+  return (
+    <nav className="container flex justify-between pt-10 sm:pt-16">
+      <Logo bold="nefarious" thin=".kas" />
+
+      <div className="hidden lg:flex justify-between w-96">
+        {
         links.map((link) => (
-          <NavLink url={link} key={link} />
+          <NavLink url={link} key={link} isActive={router.pathname === `/${link}`} />
         ))
       }
-    </div>
+      </div>
 
-    <div />
-  </nav>
-);
+      <button className="mr-10 lg:hidden" type="button">
+        <Menu className="w-7 h-7 fill-current text-gray-200" />
+      </button>
+
+      <div className="hidden lg:block" />
+    </nav>
+  );
+};
 
 export default Navbar;
