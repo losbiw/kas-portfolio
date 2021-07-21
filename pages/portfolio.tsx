d441460fc4a2174421ca27c55ef812cc0b7a6462
losbiw <skorinov41@gmail.com>
2021-07-21 22:30:21 +0300

    Uncomment actual data fetching from instagram API

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps, GetStaticPropsResult } from 'next';
-import fs from 'fs/promises';
-import { join } from 'path';
+// import fs from 'fs/promises';
+// import { join } from 'path';
 import { FC } from 'react';
 import ExamplesPanel from 'features/examples/ExamplePanel';
 import Page from '../features/Page';
@@ -25,41 +25,43 @@ const Contact: FC<Props> = ({ instagramData }: Props) => (
   </Page>
 );
 
-export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<Props>> => {
-  const data = await fs.readFile(join(__dirname, '../../../', 'dev/data.json'), 'utf8');
-  const parsedData = JSON.parse(data);
-
-  return {
-    props: {
-      instagramData: parsedData,
-    },
-  };
-};
+// this only exists for development purpose
 
 // export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<Props>> => {
-//   const instagramId = 9188954448;
-//   const instagramRes = await fetch(`https://instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"${instagramId}","first":50,"after":null}`);
-//   const json = await instagramRes.json();
-
-//   const parsedData = json.data.user.edge_owner_to_timeline_media.edges.map(({ node }: any) => {
-//     // eslint-disable-next-line camelcase
-//     const { display_url: url, edge_media_to_caption } = node;
-//     const desc = edge_media_to_caption.edges[0].node.text;
-
-//     const parsedDesc = desc.match(/[^\n#]*/)[0];
-
-//     return {
-//       url,
-//       description: parsedDesc,
-//     };
-//   });
+//   const data = await fs.readFile(join(__dirname, '../../../', 'dev/data.json'), 'utf8');
+//   const parsedData = JSON.parse(data);
 
 //   return {
 //     props: {
 //       instagramData: parsedData,
 //     },
-//     revalidate: 3600,
 //   };
 // };
+
+export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<Props>> => {
+  const instagramId = 9188954448;
+  const instagramRes = await fetch(`https://instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"${instagramId}","first":50,"after":null}`);
+  const json = await instagramRes.json();
+
+  const parsedData = json.data.user.edge_owner_to_timeline_media.edges.map(({ node }: any) => {
+    // eslint-disable-next-line camelcase
+    const { display_url: url, edge_media_to_caption } = node;
+    const desc = edge_media_to_caption.edges[0].node.text;
+
+    const parsedDesc = desc.match(/[^\n#]*/)[0];
+
+    return {
+      url,
+      description: parsedDesc,
+    };
+  });
+
+  return {
+    props: {
+      instagramData: parsedData,
+    },
+    revalidate: 3600,
+  };
+};
 
 export default Contact;
