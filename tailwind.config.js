7634003e4e000402dbe63d6b6f9f961245033d85
losbiw <skorinov41@gmail.com>
2021-07-21 03:04:03 +0300

    Extend original tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,34 @@ module.exports = {
     },
     extend: {
       borderRadius: {
+        semi: '5vmax',
         huge: '20%',
+      },
+      width: {
+        'page-80': '80vw',
+      },
+      height: {
+        'width-page-80': '80vw',
+      },
+      keyframes: {
+        open: {
+          '0%': { bottom: '100vh' },
+          '100%': { bottom: 0 },
+        },
+      },
+      animation: {
+        open: 'open 800ms linear forwards',
+        close: 'open 800ms linear reverse forwards',
+      },
+      colors: {
+        'dark-grey': '#1f1f1f',
       },
     },
   },
   variants: {
     extend: {
       fontSize: ['hover'],
+      borderRadius: ['group-hover', 'hover'],
     },
   },
   plugins: [],
