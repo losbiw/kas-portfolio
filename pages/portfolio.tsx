53dbbb1da31f3902de2f6de8ef3186c822ce9b11
losbiw <skorinov41@gmail.com>
2021-08-12 23:52:51 +0300

    Use axios instead of curl

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -2,14 +2,13 @@ import { GetStaticProps, GetStaticPropsResult } from 'next';
 import { exec as _exec } from 'child_process';
 import { promisify } from 'util';
 import { FC } from 'react';
+import axios from 'axios';
 import ExamplesPanel from 'features/examples/ExamplePanel';
 import Page from '../features/Page';
 import Title from '../features/Title';
 import pictures from '../features/examples/portfolio';
 import PostsPanel from '../features/posts/PostsPanel';
 import Post from '../types/Post.d';
-
-const exec = promisify(_exec);
 
 interface Props {
   instagramData: Post[]
@@ -32,8 +31,12 @@ export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsRe
   const fetchUrl = `https://instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=%7B%0A%20%20%22id%22%3A%20%22${instagramId}%22%2C%0A%20%20%22first%22%3A%2050%2C%0A%20%20%22after%22%3A%20null%0A%7D`;
 
   // eslint-disable-next-line max-len
-  const { stdout: data } = await exec(`curl -G "${fetchUrl}" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36" --cookie '${process.env.cookie}'`);
-  const json = JSON.parse(data);
+  const { data: json } = await axios.get(fetchUrl, {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+      cookie: process.env.cookie,
+    },
+  });
 
   const parsedData = json.data.user.edge_owner_to_timeline_media.edges.map(({ node }: any) => {
     // eslint-disable-next-line camelcase
